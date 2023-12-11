38d7a918b0848185cfaf5e4e7bbb69184d692824
rahff <raphaelandrey99@gmail.com>
2023-12-11 08:54:54 +0100

    types improvment

--- controller/transferMoneyController.ts
+++ controller/transferMoneyController.ts
@@ -4,17 +4,17 @@ import {TransfertMoneyPipeline} from "../application/TransferMoneyWorkflow.js";
 import {matchOk} from "../lib/types.js";
 
 
-type Controller = (req: Request, res: Response) => void;
+type Controller = (req: Request, res: Response) => Promise<void>;
 
 export type MoneyTransferRequest = {customerAccountId: string, destinationAccountId: string, amount: number};
 
 //CREATE CONTROLLER WITH USE CASE PIPELINE
 export const transferMoneyController = (pipeline: TransfertMoneyPipeline, transferMoney: TransferMoney): Controller => {
-    return async (req: Request, res: Response) => {
+    return async (req: Request, res: Response): Promise<void> => {
         const request: MoneyTransferRequest = {...req.body};
         const result = await pipeline(transferMoney, request);
-        if(matchOk(result)) return res.status(201).json({result: result.tag})
-        else return res.status(403).json({reject: result.value});
+        if(matchOk(result)) res.status(201).json({result: result.tag})
+        else res.status(403).json({reject: result.value});
     }
 }
 
