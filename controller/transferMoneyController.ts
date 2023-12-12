3088e145abddae92281751427c79bed519c4bcc8
rahff <raphaelandrey99@gmail.com>
2023-12-12 01:50:28 +0100

    design improvment

--- controller/transferMoneyController.ts
+++ controller/transferMoneyController.ts
@@ -1,7 +1,7 @@
 import {Request, Response} from "express";
-import {TransferMoney} from "../core/transferMoney.js";
 import {TransfertMoneyPipeline} from "../application/TransferMoneyWorkflow.js";
 import {matchOk} from "../lib/types.js";
+import {TransfertMoneyResult} from "../core/model.js";
 
 
 type Controller = (req: Request, res: Response) => Promise<void>;
@@ -12,7 +12,7 @@ export type MoneyTransferRequest = {customerAccountId: string, destinationAccoun
 export const transferMoneyController = (useCase: TransfertMoneyPipeline): Controller => {
     return async (req: Request, res: Response): Promise<void> => {
         const request: MoneyTransferRequest = {...req.body};
-        const result = await useCase(request);
+        const result: TransfertMoneyResult = await useCase(request);
         if(matchOk(result)) res.status(201).json({result: result.tag});
         else res.status(403).json({reject: result.value});
     }
